139101b97174a0bab585381f5c994c30a40ad895
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-05 22:45:46 +0530

    Add CheckOutPage, OrderPage & Tracking Page Split Header into a component and use Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import {Routes, Route} from 'react-router'
-import HomePage from './pages/HomePage'
+import HomePage from './pages/HomePage.jsx';
 import './App.css'
+import OrdersPage from './pages/OrdersPage';
+import TrackingPage from './pages/TrackingPage';
+import CheckOutPage from './pages/CheckOutPage';
 
 function App() {
   
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path='checkout' element={<div> Test </div>} />
+      <Route path='checkout' element={<CheckOutPage />} />
+      <Route path='orders' element={<OrdersPage/>} />
+      <Route path='tracking' element={<TrackingPage/>} />
     </Routes>
     
   )
