8dfb7ff33f1f9c3489e919f4bdbcb513e1434536
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-07 17:48:19 +0530

    Generate the HTML for the HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import OrdersPage from './pages/OrdersPage';
 import TrackingPage from './pages/TrackingPage';
 import CheckOutPage from './pages/checkout/CheckOutPage.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
       <Route path='checkout' element={<CheckOutPage />} />
       <Route path='orders' element={<OrdersPage/>} />
       <Route path='tracking' element={<TrackingPage/>} />
+      <Route path='*' element={<PageNotFound/>} />
     </Routes>
     
   )
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+import "./PageNotFound.css";
+
+const PageNotFound = () => {
+  return (
+    <>
+      <Header />
+      <div className="page-not-found">
+        <h1>Page Not Found</h1>
+        <p className="page-not-found-desc">
+          The page you are looking for does not exist.
+        </p> 
+      </div>
+    </>
+  );
+};
+
+export default PageNotFound;
