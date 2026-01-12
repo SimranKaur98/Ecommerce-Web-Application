3885d9cc94c746d6bdee57b7c5f9213e4806b335
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-12 18:14:34 +0530

    Fetch data from the backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,17 @@ import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 function HomePage() {
+  //Asynch code = code that does not finish right away
+  //fetch returns a promise
+  // which has a method then() which lets us wait for aysnch code to finish
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json(); //=json gives the data attached to the response, also async
+    })
+    .then((data) => { //=this .then waits for the above .then to finish and then runs.
+      console.log(data); 
+    });
+
   return (
     <>
       <title>Ecommerce Website</title>
@@ -16,10 +27,7 @@ function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -29,15 +37,18 @@ function HomePage() {
                 <div className="product-rating-container">
                   <img
                     className="product-rating-stars"
-                    src={`images/ratings/rating-${product.rating.stars*10}.png`}
+                    src={`images/ratings/rating-${
+                      product.rating.stars * 10
+                    }.png`}
                   />
-                  <div className="product-rating-count link-primary">\
-                    {product.rating.count}
+                  <div className="product-rating-count link-primary">
+                    \{product.rating.count}
                   </div>
                 </div>
 
                 <div className="product-price">
-                  ${(product.priceCents / 100).toFixed(2) } {/* convert cents to dollars*/}
+                  ${(product.priceCents / 100).toFixed(2)}{" "}
+                  {/* convert cents to dollars*/}
                 </div>
 
                 <div className="product-quantity-container">
@@ -68,7 +79,6 @@ function HomePage() {
               </div>
             );
           })}
-          
         </div>
       </div>
     </>
