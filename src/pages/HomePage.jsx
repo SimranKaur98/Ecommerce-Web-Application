a5f6b3e0838ebd40854fbe5e9d98cda202cc14f2
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-12 18:39:01 +0530

    Get cart from the backend and display quantity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,9 +8,17 @@ import "./HomePage.css";
 function HomePage() {
 
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data);
+    });
+
+    axios.get('http://localhost:3000/api/cart-items')
+    .then((response) => {
+      //cart items fetched
+      setCart(response.data);
     });
   }, []);
 
@@ -18,7 +26,7 @@ function HomePage() {
     <>
       <title>Ecommerce Website</title>
       <link rel="icon" type="image/svg+xml" href="/home-favicon.png" />
-      <Header />
+      <Header cart={cart}/>
       <div className="home-page">
         <div className="products-grid">
           {products.map((product) => {
