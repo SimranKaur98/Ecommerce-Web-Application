bf84fa5b3771353afe60afa5d1b399779258cce6
Simran KAur <simrankaurphulldevgan@gmail.com>
2026-01-12 18:44:59 +0530

    Add server proxy config for /api and /images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,11 +11,11 @@ function HomePage() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products").then((response) => {
+    axios.get("/api/products").then((response) => {
       setProducts(response.data);
     });
 
-    axios.get('http://localhost:3000/api/cart-items')
+    axios.get('/api/cart-items')
     .then((response) => {
       //cart items fetched
       setCart(response.data);
